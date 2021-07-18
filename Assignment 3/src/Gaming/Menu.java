ce505ce1e1c4ba76ff4ac4837bef76c4ce8d35d8
Muhammad Hidayat Bin Ibrahim <73527232+Dayat102@users.noreply.github.com>
2021-07-18 11:08:58 +0800

    Add files via upload

--- Assignment 3/src/Gaming/Menu.java
+++ Assignment 3/src/Gaming/Menu.java
@@ -1,6 +1,7 @@
 package Gaming;
 
 import java.awt.BorderLayout;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -8,6 +9,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.border.MatteBorder;
+
+
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -16,11 +19,13 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Toolkit;
 
-public class employe extends JFrame {
+public class Menu extends JFrame {
 
 	private JPanel contentPane;
 
@@ -31,7 +36,7 @@ public class employe extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					employe frame = new employe();
+					Menu frame = new Menu();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,8 +48,8 @@ public class employe extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public employe() {
-		setTitle("Worker Panel");
+	public Menu() {
+		setTitle("Home Menu");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 784, 491);
 		
@@ -64,37 +69,62 @@ public class employe extends JFrame {
 		});
 		mnNewMenu_1.add(mntmNewMenuItem_4);
 		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Logout");
-		mntmNewMenuItem_2.addActionListener(new ActionListener() {
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Exit");
+		mntmNewMenuItem_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Login home = new Login();
-				home.setModalExclusionType(null);
-	            home.setVisible(true);
-	            dispose();
+				System.exit(0);
 			}
 		});
-		mnNewMenu_1.add(mntmNewMenuItem_2);
+		mnNewMenu_1.add(mntmNewMenuItem_3);
 		
+		JMenu mnNewMenu = new JMenu("Product");
+		mnNewMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Advertisement");
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Advertising frame = new Advertising();
+				frame.setModalExclusionType(null);
+				frame.setVisible(true);
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Gaming Description");
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GamingDescription frame = new GamingDescription();
+				frame.setModalExclusionType(null);
+				frame.setVisible(true);
+			}
+		});
+		
+		mnNewMenu.add(mntmNewMenuItem_2);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 153, 255));
-		panel.setBorder(new MatteBorder(5, 5, 5, 5, (Color) new Color(153, 0, 153)));
+		panel.setForeground(new Color(0, 153, 204));
+		panel.setBackground(new Color(153, 204, 255));
+		panel.setBorder(new MatteBorder(5, 5, 5, 5, (Color) new Color(0, 153, 204)));
 		panel.setBounds(0, 0, 770, 432);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(255, 153, 255));
-		panel_1.setBorder(new MatteBorder(4, 4, 4, 4, (Color) new Color(153, 0, 153)));
+		panel_1.setBackground(new Color(204, 255, 255));
+		panel_1.setBorder(new MatteBorder(4, 4, 4, 4, (Color) new Color(51, 153, 204)));
 		panel_1.setBounds(73, 27, 633, 60);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Melio Administration Panel");
+		JLabel lblNewLabel = new JLabel("Welcome To Melio Enterprise");
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 35));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(10, 10, 613, 40);
@@ -103,39 +133,38 @@ public class employe extends JFrame {
 		JButton btnNewButton = new JButton("Employee");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Employee frame = new Employee();
+				Login frame = new Login();
+				frame.setModalExclusionType(null);
+				frame.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 28));
+		btnNewButton.setBounds(131, 245, 173, 52);
+		panel.add(btnNewButton);
+		
+		JButton btnCustomer = new JButton("Customer");
+		btnCustomer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Register frame = new Register();
 				frame.setModalExclusionType(null);
 	            frame.setVisible(true);
 	            dispose();
 			}
 		});
-		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 28));
-		btnNewButton.setBounds(127, 260, 173, 52);
-		panel.add(btnNewButton);
-		
-		JButton btnFinance = new JButton("Finance");
-		btnFinance.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Finance frame = new Finance();
-				frame.setModalExclusionType(null);
-				frame.setVisible(true);
-	            dispose();
-			}
-		});
-		btnFinance.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 28));
-		btnFinance.setBounds(449, 260, 173, 52);
-		panel.add(btnFinance);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("New label");
-		lblNewLabel_1_1.setIcon(new ImageIcon("C:\\Users\\ASUS\\Downloads\\WhatsApp Image 2021-06-29 at 21.03.25.jpeg"));
-		lblNewLabel_1_1.setBounds(445, 110, 177, 111);
-		panel.add(lblNewLabel_1_1);
+		btnCustomer.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 28));
+		btnCustomer.setBounds(445, 245, 173, 52);
+		panel.add(btnCustomer);
 		
 		JLabel lblNewLabel_1 = new JLabel("New label");
 		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\ASUS\\Downloads\\WhatsApp Image 2021-06-29 at 21.03.24.jpeg"));
-		lblNewLabel_1.setBounds(127, 133, 177, 66);
+		lblNewLabel_1.setBounds(127, 119, 177, 81);
 		panel.add(lblNewLabel_1);
 		
+		JLabel lblNewLabel_1_1 = new JLabel("New label");
+		lblNewLabel_1_1.setIcon(new ImageIcon("C:\\Users\\ASUS\\OneDrive - UNIVERSITY UTARA MALAYSIA\\Pictures\\download.jfif"));
+		lblNewLabel_1_1.setBounds(441, 119, 177, 81);
+		panel.add(lblNewLabel_1_1);
 		
 	}
 }
